568ba9cbd6b93e9be7934091dc0202c1b9d42213
Kranti <pkranti25@gmail.com>
2023-10-10 16:38:53 +0530

    Fresh Commit 1

---
+++ 02Oct/Test.py
@@ -0,0 +1,23 @@
+
+print("Hello World")
+print("Hello","World", sep="**") #Separator Works between 2 different words
+print("Hello World", sep="**") #Here Separator won't work because Hello World is a single string here
+print("Hello", "Kranti", end='\t') #\t is a escape sequence which ends line with a tab
+print("Next print statement not started on new line because of escape sequence")
+print(23 + 45)
+print(23 * 45)
+<<<<<<< HEAD
+print ("Not loaded in git yet")
+=======
+
+>>>>>>> 102ea663d4160773d58c9037cfba603aa16f5b5b
+print ("GIT")
+# cntrl+/  --> For Comment
+# cntrl+/  --> For Comment
+# cntrl+D   ---> For copying the code in below line
+#Source Code: Human understandable code.
+#Machine code: Machine understandable code
+
+"""
+This is multi line comment. 3 times ". 
+"""
---
+++ 09Oct/StringFunctions.py
@@ -0,0 +1,21 @@
+name = 'AaaA'  # Strings are immutable in python. They cannot be changed
+print(name)
+print(id(name))
+
+name = 'Mr BbbbbB sR'
+print(name)
+print(id(name))
+
+result1 = name.upper() #All in upper
+result2 = name.capitalize() #1st letter capital rest small
+print(result1)
+print(result2)
+print(name.title()) #first letter of every word is upper case
+print(len(name)) #print length- length starts from 1
+
+text = "Bob and Marsha and Rocky are friends and Family"
+print(text.replace(
+    
+
+
+))
