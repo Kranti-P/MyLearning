8a7e5da707deef83986254d319701286b9e01b15
Kranti <pkranti25@gmail.com>
2023-10-19 21:55:55 +0530

    Basics

--- 02Oct/Basic.py
+++ 02Oct/Basic.py
@@ -1,5 +1,5 @@
 print("Hi"*3) #Prints Hi 3 times
 
-#break: breaks the loop
+#break: breaks the loop at particular condition
 #pass: sort of skips that particular if condition but continues the loop
 #continue: continues the loop
