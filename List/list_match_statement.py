6035228553a45d24e861d169a544dd631254f8b3
Kranti <pkranti25@gmail.com>
2023-10-21 22:14:54 +0530

    List match case

--- List/list_match_statement.py
+++ List/list_match_statement.py
@@ -1,5 +1,4 @@
-from unittest import case
-
+# from unittest import case
 
 def sum_of_elements():
     y = int(input("How many elements you want to have in your list? "))
@@ -25,20 +24,21 @@ def remove_duplicates_from_list():
     print("Unique List of Elements is: ", ls1)
 
 
-def pallindrome_check():
+def palindrome_check():
     str = input("Enter a string: ")
     str_reverse = str[::-1]
-    if (str == str_reverse):
-        print("String is a pallindrome ")
+    if str == str_reverse:
+        print("String is a palindrome ")
     else:
-        print("String is NOT a pallindrome ")
+        print("String is NOT a palindrome ")
+
 
 def common_elements_in_2_lists():
-    v=int(input("How many elements are in list 1? "))
-    ls1=[int(input("Enter element for list1: ")) for i in range(v)]
-    w=int(input("How many elements are in list 2? "))
-    ls2=[int(input("Enter elements for list2: ")) for i in range(w)]
-    ls3=[]
+    v = int(input("How many elements are in list 1? "))
+    ls1 = [int(input("Enter element for list1: ")) for i in range(v)]
+    w = int(input("How many elements are in list 2? "))
+    ls2 = [int(input("Enter elements for list2: ")) for i in range(w)]
+    ls3 = []
     for i in ls1:
         if i in ls2:
             ls3.append(i)
@@ -48,7 +48,7 @@ def common_elements_in_2_lists():
 print("\n 1. Enter 1 for Sum of All Elements\n "
       "2. Enter 2 for largest element in the list\n "
       "3. Enter 3 for remove duplicates\n "
-      "4. Enter 4 for Pallindrome check \n "
+      "4. Enter 4 for Palindrome check \n "
       "5. Enter 5 for common elements in 2 lists")
 x = int(input("Enter your choice Here: "))
 
@@ -63,7 +63,7 @@ else:
         case 3:
             remove_duplicates_from_list()
         case 4:
-            pallindrome_check()
+            palindrome_check()
         case 5:
             common_elements_in_2_lists()
         case _:
